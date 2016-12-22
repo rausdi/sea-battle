9bcee4f64817f960a73ffd5fff819d5158a1c468
Roman Fedorov <rausdi@gmail.com>
2016-12-22 19:37:49 +0300

    Create a ahip abstract factory interface

---
+++ src/SeaBattle.Engine/Factories/ShipFactory/Interfaces/IShipFactory.cs
@@ -0,0 +1,12 @@
+﻿using SeaBattle.Engine.Factories.Ship;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeaBattle.Engine.Factories.ShipFactory.Interfaces
+{
+    interface IShipFactory
+    {
+        IShip Create();
+    }
+}
